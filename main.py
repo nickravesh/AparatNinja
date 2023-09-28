83932d6fa53a380291c403b953f161a30e21a83a
ZED <67244328+nickravesh@users.noreply.github.com>
2023-09-28 23:08:48 +0330

    add get_playlist_items_url function

--- main.py
+++ main.py
@@ -9,6 +9,37 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 
 
+def get_playlist_items_url(playlistURL: str):
+    # Initialize the WebDriver
+    driver = webdriver.Firefox()
+
+    # Navigate to the playlist page
+    playlist_url = "https://www.aparat.com/playlist/example"
+    driver.get(playlist_url)
+
+    # Locate the element with class "playlist-list"
+    playlist_list_element = driver.find_element(By.CLASS_NAME, "playlist-list")
+
+    # Extract video URLs from the "playlist-list" element
+    playlist_items = playlist_list_element.find_elements(By.CLASS_NAME, "playlist-item")
+
+    playlist_urls = []
+
+    for item in playlist_items:
+        # Find the <a> element within each "playlist-item" and extract the href attribute (URL)
+        video_link = item.find_element(By.TAG_NAME, "a")
+        video_url = video_link.get_attribute("href")
+        
+        playlist_urls.append(video_url)
+
+    # Print the extracted video URLs
+    for url in playlist_urls:
+        print(url)
+
+    # Close the WebDriver when done
+    driver.quit()
+
+
 def get_video_url(videoUrl: str, videoQuality: str) -> str:
 
     # enable the firefox options to set arguments
