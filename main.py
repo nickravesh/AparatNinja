ef271b4dccaf272fea8b199a4a8a709802123d6c
ZED <67244328+nickravesh@users.noreply.github.com>
2023-09-30 22:27:13 +0330

    update main.py

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+from colorama import Fore
 import getPlaylistItems
 import getVideoDownloadURL
 import downloadVideo
 
 # get playlist items url
-userPlaylistURL = input("Provide PlaylistURL:")
+userPlaylistURL = input(f"{Fore.LIGHTMAGENTA_EX}Enter The Playlist URL:{Fore.RESET}\n")
+userSelectedQuality = input(f"{Fore.LIGHTYELLOW_EX}Enter The Quality You Want To Download Videos:{Fore.RESET}\n")
 listOfURLs = getPlaylistItems.get_playlist_items_url(userPlaylistURL)
 
 directDownloadLinkWithTitle = []
 for item in listOfURLs:
-    print("found a video:")
-    print(item)
-
     #directDownloadLinkWithTitle.append(getVideoDownloadURL.get_video_download_url(item, "144p"))
-    downloadLink_videoTitle = getVideoDownloadURL.get_video_download_url(item, "144p")
-    downloadVideo.download_video(downloadLink_videoTitle[0], downloadLink_videoTitle[1])
+    directDownloadLinkAndVideoTitle = getVideoDownloadURL.get_video_download_url(item, userSelectedQuality)
+    downloadVideo.download_video(directDownloadLinkAndVideoTitle[0], directDownloadLinkAndVideoTitle[1])
+
+
+
+# TODO: fix firefox webdriver not fully exit in case of an error. fix it with try except that in except in quite the driver
