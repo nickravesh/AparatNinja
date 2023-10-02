43ef8fa7190a7dabc860c18cf51137c4a02918f5
ZED <67244328+nickravesh@users.noreply.github.com>
2023-10-02 13:16:51 +0330

    [feat]: Add module to check available video qualities from a given URL

---
+++ checkAvailableQualities.py
@@ -0,0 +1,24 @@
+import re
+import requests
+
+def check_available_qualities(directDownloadURL: str) -> list:
+    allPossibleQualities = []
+    choices = ["144p", "240p", "320p", "360p", "480p", "720p", "1080p"] # TODO: expand the quality list
+    for item in choices:
+        allPossibleQualities.append(directDownloadURL.replace("144p", item))
+
+    validQualities = []
+    for url in allPossibleQualities:
+        if requests.get(url, stream=True).status_code == 200:
+            validQualities.append(url)
+        else:
+            print(f"the URL {url[50]} has discarded")
+
+
+    validQualitiesTags = []
+    for url in validQualities:
+        validQualitiesTags.append(re.search("\d+p", url).group(0))
+
+
+    return validQualitiesTags
+
